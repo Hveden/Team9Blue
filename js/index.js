4e56a006d2838123d287c8d156681e37d1e2b2d5
Hveden <tobias.ciesla@hotmail.com>
2018-09-06 10:50:04 +0200

    hej

--- js/index.js
+++ js/index.js
@@ -56,11 +56,12 @@ function refreshDeviceList(){
 
 function onDiscoverDevice(device){
 	//Make a list in html and show devises
+    if(device.name == "Nevad"){
 		var listItem = document.createElement('li'),
 		html = device.name+ "," + device.id;
 		listItem.innerHTML = html;
 		document.getElementById("bleDeviceList").appendChild(listItem);
-
+    }
 }
 
 
