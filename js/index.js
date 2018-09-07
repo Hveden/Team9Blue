9755a6c87005fcfdd505770583f098ad63cd41aa
KB <43027028+KB@users.noreply.github.com>
2018-09-07 09:44:23 +0200

    If-statement ændring
    Jeg havde glemt ")"

--- js/index.js
+++ js/index.js
@@ -56,7 +56,7 @@ function refreshDeviceList(){
 
 function onDiscoverDevice(device){
 	//Make a list in html and show devises
-		if(device.name == "HVEDEN"
+		if(device.name == "HVEDEN")
 		{
 		var listItem = document.createElement('li'),
 		html = device.name+ "," + device.id;
